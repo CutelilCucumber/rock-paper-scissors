bc2c721ab487a36b33e73798b4cb98183279a146
CutelilCucumber <sond.brian@gmail.com>
2025-03-01 13:59:16 -0800

    finish rock paper scissors assignment
    call created functions in a loop to play 5 rounds of rock paper scissors

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 
 function getComputerChoice(){ //for computer choice
     let ran = Math.random() * 3; //chose a number from 0-2
-    console.log(ran);
     if(ran <= 1) {
         return 'rock'; //if ran is 0 return rock
     } if (ran <=2){
@@ -17,41 +16,65 @@ function getHumanChoice(){ //the players' choice
         answer = prompt('Rock, paper, or scissors?'); //ask for input & define var
         answer = answer.toLowerCase(); //convert input to lowercase for condition
         if (answer == 'rock' || answer == 'paper' || answer == 'scissors'){
-            return answer;
+            return answer;// leave the loop & function
         }
     }
 }
-function playRound(compAnswer, humanAnswer){ //start of the round
-    let compAnswer = getComputerChoice(); //get comp answer
-    let humanAnswer = getHumanChoice(); //get human answer
+function playRound(compChoice, humanChoice){ //start of the round
+    compChoice = getComputerChoice(); //get comp answer
+    humanChoice = getHumanChoice(); //get human answer
+    console.log('Computer plays '+ compChoice);//reply comp answer
+    console.log('Human plays '+ humanChoice);//reply human answer
     let winner = '';
-    if(compAnswer == humanAnswer){  //in case of tie
-
-    }    
+    let winCond = compChoice+humanChoice;
+    if(compChoice == humanChoice){  //in case of tie
+        return 'Tie';
+    }if (winCond == 'rockscissors'||winCond == 'scissorspaper'||winCond == 'paperrock') { //determine loss possibiliteis
+        return 'Computer'; // in case of loss
+    } else {
+        return 'Human'; // in case of win
+    }
     //return winner
 
 }
 
 
-console.log(compAnswer); //display computer answer
+// console.log(compAnswer); //display computer answer
 
-console.log(humanAnswer); // display human answer
-
+// console.log(humanAnswer); // display human answer
+let compAnswer = ''//initialize computer response
+let humanAnswer = ''//initialize human response
 let compScore = 0; //initialize computer score
 let humanScore = 0; //initialize human score
+let winner = ""//initialize winner
 
 for(let i = 0; i < 5; i++){ //game runs for 5 rounds
     
+    // compAnswer = getComputerChoice();//computer var asks the function
+    // humanAnswer = getHumanChoice();//human var asks the function
+    winner = playRound(compAnswer, humanAnswer);//variables go head to head
+    
+    if (winner == 'Computer') {
+        compScore++;
+        console.log(winner+' won the round');//display round winner
+    } if (winner == 'Human') {
+        humanScore++;
+        console.log(winner+' won the round');//display round winner
+    } else {
+        console.log('Nobody Won!');
+    }
+    console.log ('PC: ' + compScore + '| You: ' + humanScore); //announces scores
 }
 
-if(compScore < humanScore){
-    console.log('You win!');
+if(compScore < humanScore){ 
+    console.log('You win!'); // announce winner
 
 }if (compScore > humanScore) {
     console.log('You lose!');
 
-} else {
-    console.log('You tied!');
+}
+if (compScore == humanScore){
+   console.log('You tied!');
     
 }
 
